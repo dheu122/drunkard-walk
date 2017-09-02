7a387e8b1273efd44e5d442aea80cf216be3244c
David <davidheu@ymail.com>
2017-09-01 23:01:28 -0700

    inital commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var Config = {
+    gridWidth: 100,
+    gridHeight: 100,
+
+    roomWidthMin: 3,
+    roomWidthMax: 3,
+    roomHeightMin: 3,
+    roomHeightMax: 3,
+    
+    agentRoomPlacementPercentage: 0.25,
+    agentDirectionChangePercentage: 0.10,
+}
---
+++ generator.js
@@ -0,0 +1,17 @@
+var Generator = {
+    Grid: function(options) {
+
+        this.init = function() {
+            // Inits a clean grid with a randomized agent placement and options reset
+        }
+
+        this.generate = function() {
+            // Starts moving the agent depending on options
+        }
+
+        this.render = function() {
+            // Renders the canvas depending on the grid
+        }
+
+    }
+}
